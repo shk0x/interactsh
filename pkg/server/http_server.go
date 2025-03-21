9cc662859dde59d4935feaa46044f84a7d1ae252
shk0x <sshk0x@gmail.com>
2025-03-21 01:27:05 -0300

    fix interact

--- pkg/server/http_server.go
+++ pkg/server/http_server.go
@@ -261,7 +261,7 @@ func (h *HTTPServer) defaultHandler(w http.ResponseWriter, req *http.Request) {
 
 	reflection := h.options.URLReflection(req.Host)
 	if stringsutil.HasPrefixI(req.URL.Path, "/") && h.staticHandler != nil {
-		fmt.Println("**********HasPrefixI**********")
+		fmt.Println("**********HasPrefixI**********2")
 		h.staticHandler.ServeHTTP(w, req)
 	} else if req.URL.Path == "/" && reflection == "" {
 		fmt.Println("**********eq.URL.Path == / && reflection == **********")
